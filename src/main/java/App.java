386d7622032da41bca3888150a41fb448cc46ea2
李海军 <754491577@qq.com>
2019-06-19 19:33:26 +0800

    初始化项目

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+/**
+ * @ProjectName: activiti-design
+ * @Package: PACKAGE_NAME
+ * @ClassName: App
+ * @Author: Administrator
+ * @Description: ${description}
+ * @Date: 2019/6/19 19:31
+ * @Version: 1.0
+ */
+public class App {
+}
